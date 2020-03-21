c9c999b3bc3c5734a126d4c491a17ed7d023c5be
Sebastien Flochlay <sebastien.flochlay@stack-labs.com>
2020-03-21 22:50:40 +0100

    feat(core): add a neested component example

--- cli/hello-world/src/App.vue
+++ cli/hello-world/src/App.vue
@@ -2,16 +2,19 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <NesteedComponent />
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import NesteedComponent from './components/NesteedComponent.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    NesteedComponent
   }
 }
 </script>
---
+++ cli/hello-world/src/components/NesteedComponent.vue
@@ -0,0 +1,36 @@
+<template>
+  <nav class="nesteed-component">
+    <h2>Component parts:</h2>
+    <ul>
+      <li><a href="">Home</a></li>
+      <li><a href="">About</a></li>
+      <li><a href="">Contact</a></li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'NesteedComponent',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style>
+nav.nesteed-component{
+  text-align: center;
+  background-color: aliceblue;
+}
+nav.nesteed-component ul{
+  padding: 0;
+}
+nav.nesteed-component li{
+  display: inline-block;
+  list-style-type: none;
+  margin: 10px;
+}
+</style>
