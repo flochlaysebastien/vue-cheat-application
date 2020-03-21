6965123aa86039be13427a62d8a6e10a71d5ca84
Sebastien Flochlay <sebastien.flochlay@stack-labs.com>
2020-03-21 10:30:51 +0100

    feat(core): add vue event example

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
 new Vue({
     el: '#app',
     data: {
-      title: 'Basic Vue.js instance example',
-      god: 'Cthulhu',
-      url: 'http://www.youtube.com',
-      classes: ['one', 'two']
+        title: 'Basic Vue.js instance example',
+        god: 'Cthulhu',
+        url: 'http://www.youtube.com',
+        classes: ['one', 'two'],
+        wage: 10
     },
     methods: {
-        hello(who){
+        hello(who) {
             return `Hello, ${who} !!`
         },
-        pray(){
+        pray() {
             return `Iä ! Iä ! ${this.god} fhtagn !`
+        },
+        changeWage(amount) {
+            this.wage += amount
         }
     }
-  })
+})
