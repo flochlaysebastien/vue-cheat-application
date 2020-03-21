e74b3ff0c9edfdfe6581fbcb8fe771fdae7d0195
Sebastien Flochlay <sebastien.flochlay@stack-labs.com>
2020-03-21 23:18:34 +0100

    feat(core): add a life-cycle hooks example

--- cli/hello-world/src/components/NesteedComponent.vue
+++ cli/hello-world/src/components/NesteedComponent.vue
@@ -1,5 +1,6 @@
 <template>
   <nav class="nesteed-component">
+    <h2 @click="changeTitle">{{ title }}</h2>
     <h2>Component parts:</h2>
     <ul>
       <li><a href="">Home</a></li>
@@ -14,8 +15,25 @@ export default {
   name: 'NesteedComponent',
   data () {
     return {
-
+      title: 'Nesteed Component v1'
     }
+  },
+  methods: {
+    changeTitle(){
+      this.title = 'Nesteed Component v2'
+    }
+  },
+  beforeCreate(){
+    alert('beforeCreate hook')
+    console.log('beforeCreate hook')
+  },
+  created(){
+    alert('created hook')
+    console.log('created hook')
+  },
+  beforeUpdate(){
+    alert('beforeUpdate hook')
+    console.log('beforeUpdate hook')
   }
 }
 </script>
