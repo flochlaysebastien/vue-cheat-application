76187979e77e3aa3356ff60672b01c3bbe8618ef
Sebastien Flochlay <sebastien.flochlay@stack-labs.com>
2020-03-21 22:28:09 +0100

    feat(core): add vue data function example

--- cli/hello-world/src/components/HelloWorld.vue
+++ cli/hello-world/src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <h1>{{ title }}</h1>
+    <h2>{{ msg }}</h2>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
@@ -35,7 +36,12 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
-  }
+  },
+  data() {
+    return {
+      title: 'Hello, world !'
+    }
+  },
 }
 </script>
 
