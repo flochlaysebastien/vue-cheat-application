72ed049b4c147265f15321b99ede7fd1eba80746
Sebastien Flochlay <sebastien.flochlay@stack-labs.com>
2020-03-21 23:37:17 +0100

    feat(core): add global filter example

--- cli/hello-world/src/main.js
+++ cli/hello-world/src/main.js
@@ -1,7 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// create global config
 Vue.config.productionTip = false
+
+// create global filter
+Vue.filter('shorter', val => {
+  if(!val || typeof(val) != 'string') return ''
+  val = val.slice(0, 50) + '...'
+  return val
+})
 
 new Vue({
   render: h => h(App),
