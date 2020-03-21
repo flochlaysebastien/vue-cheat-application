aa43fb37b311b634731f07729b2e8f2081c3af00
Sebastien Flochlay <sebastien.flochlay@stack-labs.com>
2020-03-21 10:02:22 +0100

    feat(core): add vue methods example

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 new Vue({
     el: '#app',
     data: {
-      title: 'Basic Vue.js instance example'
+      title: 'Basic Vue.js instance example',
+      god: 'Cthulhu'
+    },
+    methods: {
+        hello(who){
+            return `Hello, ${who} !!`
+        },
+        pray(){
+            return `Iä ! Iä ! ${this.god} fhtagn !`
+        }
     }
   })
